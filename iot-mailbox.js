5ec32f4f222c6373f0f9f3686df5cc85a363ad8f
Vanja <vanja.divkovic@gmail.com>
2016-06-16 22:43:15 +0200

    Thing name as command line arg -T

--- iot-mailbox.js
+++ iot-mailbox.js
@@ -19,47 +19,47 @@ const newMailTopic = 'mailboxpi/newmail';
 
 //begin module
 function run(args) {
-   //
-   // Instantiate the thing shadow class. For now shadows are not used.
-   //
-   const thingShadows = thingShadow({
-      keyPath: args.privateKey,
-      certPath: args.clientCert,
-      caPath: args.caCert,
-      clientId: args.clientId,
-      region: args.region,
-      baseReconnectTimeMs: args.baseReconnectTimeMs,
-      keepalive: args.keepAlive,
-      protocol: args.Protocol,
-      port: args.Port,
-      host: args.Host,
-      debug: args.Debug
-   });
+    //
+    // Instantiate the thing shadow class.
+    //
+    const thingShadows = thingShadow({
+        keyPath: args.privateKey,
+        certPath: args.clientCert,
+        caPath: args.caCert,
+        clientId: args.clientId,
+        region: args.region,
+        baseReconnectTimeMs: args.baseReconnectTimeMs,
+        keepalive: args.keepAlive,
+        protocol: args.Protocol,
+        port: args.Port,
+        host: args.Host,
+        debug: args.Debug
+    });
 
-   thing.runThing(thingShadows)
-   console.log("testMode: " + args.testMode);
+    thing.runThing(thingShadows, args.thingName)
+    console.log("Run thingName: " + args.thingName + " testMode: " + args.testMode);
 
-   function sendNotification(){
-     var msg = JSON.stringify({
-       thingName: thing.thingName,
-       serialNumber: 12345,
-       newMailAt: new Date().getTime()
-     });
-     console.log("New mail notificaton sent: " + msg);
-     thingShadows.publish(newMailTopic, msg);
-   }
+    function sendNotification() {
+        var msg = JSON.stringify({
+            thingName: args.thingName,
+            serialNumber: 12345,
+            newMailAt: new Date().getTime()
+        });
+        console.log("New mail notificaton sent: " + msg);
+        thingShadows.publish(newMailTopic, msg);
+    }
 
-   // testMode 2 is using sensor
-   if (args.testMode === 2) {
-     sensor.runSensor(sendNotification);
-   } else {
-     setTimeout(sendNotification, 5000);
-   }
+    // testMode 2 is using sensor
+    if (args.testMode === 2) {
+        sensor.runSensor(sendNotification);
+    } else {
+        setTimeout(sendNotification, 5000);
+    }
 } // end run
 
 module.exports = cmdLineProcess;
 
 if (require.main === module) {
-   cmdLineProcess('connect to the AWS IoT service send notification on new mail',
-      process.argv.slice(2), run);
+    cmdLineProcess('connect to the AWS IoT service send notification on new mail',
+        process.argv.slice(2), run);
 }
--- thing.js
+++ thing.js
@@ -1,11 +1,11 @@
 const thingShadow = require('aws-iot-device-sdk').thingShadow;
 const os = require('os');
 
-const thingName = 'MailboxPi';  // same as created in AWS IoT Console
-
-function runThing(thingShadows) {
+function runThing(thingShadows, thingName) {
     const operationTimeout = 10000; // ms
     var currentTimeout = null;
+
+    console.log('runThing thingName: ' + thingName);
 
     //
     // Stack of client token values returned from thingShadows.update()
@@ -17,143 +17,142 @@ function runThing(thingShadows) {
     var tokenStack = [];
 
     function genericOperation(operation, state) {
-       var clientToken = thingShadows[operation](thingName, state);
+        var clientToken = thingShadows[operation](thingName, state);
 
-       if (clientToken === null) {
-          //
-          // The thing shadow operation can't be performed because another one
-          // is pending; if no other operation is pending, reschedule it after an
-          // interval which is greater than the thing shadow operation timeout.
-          //
-          if (currentTimeout !== null) {
-             console.log('operation in progress, scheduling retry...');
-             currentTimeout = setTimeout(
-                function() {
-                   genericOperation(operation, state);
-                },
-                operationTimeout * 2);
-          }
-       } else {
-          //
-          // Save the client token so that we know when the operation completes.
-          //
-          tokenStack.push(clientToken);
-       }
+        if (clientToken === null) {
+            //
+            // The thing shadow operation can't be performed because another one
+            // is pending; if no other operation is pending, reschedule it after an
+            // interval which is greater than the thing shadow operation timeout.
+            //
+            if (currentTimeout !== null) {
+                console.log('operation in progress, scheduling retry...');
+                currentTimeout = setTimeout(
+                    function() {
+                        genericOperation(operation, state);
+                    },
+                    operationTimeout * 2);
+            }
+        } else {
+            //
+            // Save the client token so that we know when the operation completes.
+            //
+            tokenStack.push(clientToken);
+        }
     }
 
     function getState() {
-      return {
-        state: {
-            desired: {
-              thingName: thingName,
-              hostName: os.hostname(),
-              avgLoad: os.loadavg(),
-              uptime: os.uptime(),
-              freemem: os.freemem(),
-              netInterfaces: os.networkInterfaces()
+        return {
+            state: {
+                desired: {
+                    thingName: thingName,
+                    hostName: os.hostname(),
+                    avgLoad: os.loadavg(),
+                    uptime: os.uptime(),
+                    freemem: os.freemem(),
+                    netInterfaces: os.networkInterfaces()
+                }
             }
-         }
-      }
+        }
 
     }
 
     function register() {
-       thingShadows.register(thingName, {
-          ignoreDeltas: true,
-          operationTimeout: operationTimeout
-       });
-       genericOperation('update', getState());
+        thingShadows.register(thingName, {
+            ignoreDeltas: true,
+            operationTimeout: operationTimeout
+        });
+        genericOperation('update', getState());
     }
 
     function handleStatus(thingName, stat, clientToken, stateObject) {
-       var expectedClientToken = tokenStack.pop();
+        var expectedClientToken = tokenStack.pop();
 
-       if (expectedClientToken === clientToken) {
-          console.log('got \'' + stat + '\' status on: ' + thingName);
-       } else {
-          console.log('(status) client token mismtach on: ' + thingName);
-       }
+        if (expectedClientToken === clientToken) {
+            console.log('got \'' + stat + '\' status on: ' + thingName);
+        } else {
+            console.log('(status) client token mismtach on: ' + thingName);
+        }
 
         console.log('updated state to thing shadow');
         //
         // If no other operation is pending, restart it after 60 seconds.
         //
         if (currentTimeout === null) {
-           currentTimeout = setTimeout(function() {
-              currentTimeout = null;
-              genericOperation('update', getState());
-           }, 60000);
+            currentTimeout = setTimeout(function() {
+                currentTimeout = null;
+                genericOperation('update', getState());
+            }, 60000);
         }
     }
 
     function handleDelta(thingName, stateObject) {
-          console.log('unexpected delta: ' + thingName);
+        console.log('unexpected delta: ' + thingName);
     }
 
     function handleTimeout(thingName, clientToken) {
-       var expectedClientToken = tokenStack.pop();
+        var expectedClientToken = tokenStack.pop();
 
-       if (expectedClientToken === clientToken) {
-          console.log('timeout on: ' + thingName);
-       } else {
-          console.log('(timeout) client token mismtach on: ' + thingName);
-       }
+        if (expectedClientToken === clientToken) {
+            console.log('timeout on: ' + thingName);
+        } else {
+            console.log('(timeout) client token mismtach on: ' + thingName);
+        }
 
-       genericOperation('update', getState());
+        genericOperation('update', getState());
     }
 
     // Register shadow callbacks
     thingShadows.on('connect', function() {
-       console.log('connected to AWS IoT');
-       register();
+        console.log('connected to AWS IoT');
+        register();
     });
 
     thingShadows.on('close', function() {
-       console.log('close');
-       thingShadows.unregister(thingName);
+        console.log('close');
+        thingShadows.unregister(thingName);
     });
 
     thingShadows.on('reconnect', function() {
-       console.log('reconnect');
+        console.log('reconnect');
     });
 
     thingShadows.on('offline', function() {
-       //
-       // If any timeout is currently pending, cancel it.
-       //
-       if (currentTimeout !== null) {
-          clearTimeout(currentTimeout);
-          currentTimeout = null;
-       }
-       //
-       // If any operation is currently underway, cancel it.
-       //
-       while (tokenStack.length) {
-          tokenStack.pop();
-       }
-       console.log('offline');
+        //
+        // If any timeout is currently pending, cancel it.
+        //
+        if (currentTimeout !== null) {
+            clearTimeout(currentTimeout);
+            currentTimeout = null;
+        }
+        //
+        // If any operation is currently underway, cancel it.
+        //
+        while (tokenStack.length) {
+            tokenStack.pop();
+        }
+        console.log('offline');
     });
 
     thingShadows.on('error', function(error) {
-       console.log('error', error);
+        console.log('error', error);
     });
 
     thingShadows.on('message', function(topic, payload) {
-       console.log('message', topic, payload.toString());
+        console.log('message', topic, payload.toString());
     });
 
     thingShadows.on('status', function(thingName, stat, clientToken, stateObject) {
-       handleStatus(thingName, stat, clientToken, stateObject);
+        handleStatus(thingName, stat, clientToken, stateObject);
     });
 
     thingShadows.on('delta', function(thingName, stateObject) {
-       handleDelta(thingName, stateObject);
+        handleDelta(thingName, stateObject);
     });
 
     thingShadows.on('timeout', function(thingName, clientToken) {
-       handleTimeout(thingName, clientToken);
+        handleTimeout(thingName, clientToken);
     });
 }
 
 exports.runThing = runThing;
-exports.thingName = thingName
