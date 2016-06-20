30001fd5d9c0886888af31752a21f4a6b486651f
Vanja <vanja.divkovic@gmail.com>
2016-06-21 00:35:40 +0200

    Motion sensor support added

--- sensor.js
+++ sensor.js
@@ -1,23 +1,19 @@
-// TODO remove later when real sensors are added
-// TODO change to event EventEmitter
+var raspi = require('raspi-io');
+var five = require('johnny-five');
 
 function runSensor(newMailCb) {
-     var stdin = process.stdin;
-     stdin.setRawMode( true );
-     // resume stdin in the parent process (node app won't quit all by itself
-     // unless an error or process.exit() happens)
-     stdin.resume();
-     stdin.setEncoding( 'utf8' );
+    var board = new five.Board({
+        io: new raspi()
+    });
 
-     // on any data into stdin
-     stdin.on( 'data', function( key ){
-       // ctrl-c ( end of text )
-       if ( key === '\u0003' ) {
-         process.exit();
-       }
-       newMailCb();
-     });
-   }
+    board.on('ready', function() {
 
+        var motion = new five.Motion('P1-7');
+
+        motion.on('motionstart', function() {
+            newMailCb();
+        })
+    });
+}
 
 module.exports.runSensor = runSensor;
